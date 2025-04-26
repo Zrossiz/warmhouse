b99cb8df3f87674eac64eb51ea2a4237e4b1d454
Zrossiz <soon.pluxury@mail.ru>
2025-04-26 17:52:56 +0300

    chore: added dockerfile

--- apps/temperature_api/main.go
+++ apps/temperature_api/main.go
@@ -25,20 +25,55 @@ func main() {
 
 func handler(rw http.ResponseWriter, r *http.Request) {
 	path := r.URL
-	if strings.Contains("/temperature", path.String()) {
-		data := responseDTO{
-			Value: rand.IntN(100),
-		}
-
-		rw.Header().Set("Content-Type", "application/json")
-		rw.WriteHeader(http.StatusOK)
-
-		if err := json.NewEncoder(rw).Encode(data); err != nil {
-			http.Error(rw, "Internal Server Error", http.StatusInternalServerError)
-		}
+	isValidPattern := strings.Contains("/temperature", path.String())
+	if isValidPattern && r.Method == "GET" {
+		query := r.URL.Query()
+		location := query.Get("location")
+		sensorId := query.Get("sensorId")
+		handleRoute(location, sensorId, rw)
 		return
 	}
 
 	rw.Header().Set("Content-Type", "application/json")
 	rw.WriteHeader(http.StatusNotFound)
+}
+
+func handleRoute(location string, sensorID string, rw http.ResponseWriter) {
+	data := responseDTO{
+		Value: rand.IntN(100),
+	}
+
+	if location == "" {
+		switch sensorID {
+		case "1":
+			location = "Living Room"
+		case "2":
+			location = "Bedroom"
+		case "3":
+			location = "Kitchen"
+		default:
+			location = "Unknown"
+		}
+	}
+
+	if sensorID == "" {
+		switch location {
+		case "Living Room":
+			sensorID = "1"
+		case "Bedroom":
+			sensorID = "2"
+		case "Kitchen":
+			sensorID = "3"
+		default:
+			sensorID = "0"
+		}
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(rw).Encode(data); err != nil {
+		http.Error(rw, "Internal Server Error", http.StatusInternalServerError)
+	}
+	return
 }
